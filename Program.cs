1f2e305fb2d1f999bf9b6321ecaa278e54cc68c9
Oleg Golenischev <oleg-golenischev63@yandex.ru>
2022-04-25 13:02:05 +0300

    First project create

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("What is your name? ");
+string name = Console.ReadLine() ?? "";
+Console.Write("How old are you? ");
+int age = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine($"Your name is {name}. Your are {age} years old.");
